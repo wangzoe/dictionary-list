69bc96c36e268e708add2321d9db42c1bd74733f
wangzoe <zoewang.z@qq.com>
2017-03-05 00:09:50 +0800

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,17 @@
 #dictionary & list
 
 from random import randint
-contact=[info1,info2,info3]
 info1={}
 info2={}
 info3={}
+contact=[info1,info2,info3]
+
 for i in range(0,3):
     contact[i]['name']=input('enter your name: ')
     contact[i]['age']=eval(input('enter your age: '))
     contact[i]['code']=randint(0,10)
     print(contact[i])
+print(contact)
 
 print('Please enter your certification information as request')
 name=input('First name: ')
@@ -28,4 +30,4 @@ for contact_info in contact[:]:
 if n==True and a==True:
     print('Your code is '+str(contact_info['code']))
 else:
-    print("We couldn't find your information.")       
+    print("We couldn't find your information.") 
